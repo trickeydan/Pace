5448893bff3d2c27f22521bb999d9326d3ae8fad
Dan Trickey <dan@trickey.xyz>
2016-04-02 10:32:50 +0100

    Fixed Typo in config file looking for definition that didn't exist.

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST'),
-            'database'  => env('DB_DATBASE'),
+            'database'  => env('DB_DATABASE'),
             'username'  => env('DB_USERNAME'),
             'password'  => env('DB_PASSWORD'),
             'charset'   => 'utf8',
