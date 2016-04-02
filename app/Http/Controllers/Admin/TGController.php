64bcda50ccc134b48066355c304406ef4691d557
Dan Trickey <dan@trickey.xyz>
2016-04-02 14:57:28 +0100

    Fixed it showing a blank page when a tg was access that didn't exist.

--- app/Http/Controllers/Admin/TGController.php
+++ app/Http/Controllers/Admin/TGController.php
@@ -10,6 +10,7 @@ use Pace\Tutorgroup;
 class TGController extends Controller
 {
     public function view(Tutorgroup $tg,Request $request){
+        if($tg->name == null) abort(404);
         return view('admin.tgview',[
             'tg' => $tg,
             'request' => $request,
