3cecc2910912570429c4a7aa0c117d4568f86c0d
Dan Trickey <dan@trickey.xyz>
2016-04-04 20:51:40 +0100

    Spam commit

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,1 @@
-<h6 class="text-muted text-center"><small>&copy;<?php echo date('Y');?> <a href="https://trickey.xyz">D.Trickey</a>&nbsp;<a href="{{env('BUGTRACKER')}}">Bug Tracker</a>&nbsp;v{{Illuminate\Support\Facades\Config::get('app.version')}}</small></h6>
+<h6 class="text-muted text-center"><small>&copy;<?php echo date('Y');?> <a href="https://trickey.xyz">D.Trickey</a>&nbsp;<a href="{{env('BUGTRACKER')}}">Breast Enlargement CHEAP HERE</a>&nbsp;v{{Illuminate\Support\Facades\Config::get('app.version')}}</small></h6>
