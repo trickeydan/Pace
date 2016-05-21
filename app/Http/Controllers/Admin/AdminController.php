366f2d37bf80db97c1566fcee5996d66106a9cf1
Dan Trickey <dan@trickey.xyz>
2016-05-21 19:17:59 +0100

    Added usage statistics page

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,10 +4,13 @@ namespace Pace\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
 use Pace\Http\Requests;
 use Pace\Http\Controllers\Controller;
 use Pace\Feedback;
 use Pace\ImportManager;
+use Pace\User;
+use Pace\Log;
 
 class AdminController extends Controller
 {
@@ -16,6 +19,12 @@ class AdminController extends Controller
     }
 
     public function usage(){
-        return 'USAGE';
+        $amountunique = DB::select('SELECT count( DISTINCT (`user_id`) ) AS `amount` FROM LOGS ')[0]->amount;
+        return view('admin.usage',[
+            'amountunique' => $amountunique,
+            'percentageunique' => round( ($amountunique *100) / User::whereUserLevel(1)->count(),3),
+            'totalhits' => Log::all()->count(),
+            'hitstoday' => DB::table('logs')->select(DB::raw('*'))->whereRaw('Date(created_at) = CURDATE()')->count(),
+        ]);
     }
 }
