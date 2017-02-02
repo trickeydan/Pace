f5c52e90bf9ffac31be1d9e1b0e809feead290f8
Dan Trickey <dan@trickey.xyz>
2017-02-02 12:04:38 +0000

    Fixed rendering error on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,44 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="{{asset('css/login.css')}}">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="{{asset('css/login.css')}}">
+    </head>
 
-<body>
-<div class="login-dark">
-    <form role="form" method="POST" action="{{ url('/login') }}">
-        {{ csrf_field() }}
-        <h2 class="sr-only">Login Form</h2>
-        <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
-
-        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-            @if ($errors->has('email'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-            @endif
+    <body>
+        <div class="login-dark">
+            <form role="form" method="POST" action="{{ url('/login') }}">
+                <h2 class="sr-only">Login Form</h2>
+                {{ csrf_field() }}
+                <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <input class="form-control" id="email" required autofocus type="email" name="email" placeholder="Email" value="{{ old('email') }}" >
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <input class="form-control" id="password" type="password" name="password" required placeholder="Password">
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block" type="submit">Log In</button>
+                </div><a href="{{ url('/password/reset') }}" class="forgot">Forgot your email or password?</a></form>
         </div>
-        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <input id="password" type="password" class="form-control" name="password" required>
-
-            @if ($errors->has('password'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
-        </div>
-        <div class="form-group">
-            <button class="btn btn-primary btn-block" type="submit">Log In</button>
-        </div><a href="{{ url('/password/reset') }}" class="forgot">Forgot your email or password?</a></form>
-</div>
-<script src="{{asset('js/app.js')}}"></script>
-</body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
 
 </html>
