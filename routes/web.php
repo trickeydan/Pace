0aabfd7a3f736bdea019eb4c38aa712e495257e2
Dan Trickey <dan@trickey.xyz>
2017-02-02 12:59:09 +0000

    Switched to sass and cdn for formatting
    Also added login redirect and some tests

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ *  This file defines the routes for the application.
+ *
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('auth.login');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('logout', 'Auth\LoginController@logout')->name('auth.logout');;
 
-Auth::routes();
+// Password Reset Route
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('auth.password');;
 
-Route::get('/home', 'HomeController@index');
+
+// Main Routes
+Route::get('/', 'HomeController@index')->name('home');
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\User;
+
+class LoginTest extends TestCase
+{
+    /**
+     *  This test checks if the user is redirected when visiting the home page without being logged in.
+     *
+     * @return void
+     */
+    public function testHomePageRedirect()
+    {
+
+        // Now check if we can visit the home page.
+
+        $response = $this->get('/');
+        $this->assertNotFalse($response->isRedirection(),'User not redirected to login page if not logged in.');
+        $response->assertStatus(302,'Bad status code on redirect.');
+
+    }
+
+    /**
+     * This test checks to see if we can visit the login page.
+     *
+     * @return void
+     */
+
+    public function testCanVisitLoginPage(){
+        $response = $this->get(route('auth.login'));
+        $response->assertStatus(200,'Could not visit login page').
+        $response->assertSee('Log In');
+    }
+    
+}
